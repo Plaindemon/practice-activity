6bc99b8ef4384a9e349de265b0dab4011bee8fe3
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-14 10:58:39 -0700

    not sure if this will fix the issue but i replaced the code

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import "./utils/strava";
 import polyline from '@mapbox/polyline'
 
 function App() {
-  const client_id = '77814';
-  const client_secret = 'ba4cf64706994d406df016b09df6d62ee55edaef';
-  const refresh_token = 'e3c3bce7513bb09b9c19bfd2450855830fb0d313';
+
+    // useEffect(() => {
+    // //  console.log('useEffect ran successfully')
 
 
-    useEffect(() => {
-    //  console.log('useEffect ran successfully')
-/// test add 
-
-    },
-      []);
+    // },
+    //   []);
   return (
     <div className="app">
       <h1>Strava Activities</h1>
+      <div className="activity" id="activity"></div>
       <div className="activity" id="activity-name"></div>
       <div id="activity-distance"></div>
       <div id="averageSpeed"></div>
